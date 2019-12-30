66726664226b756730cb8263caf0e13987c02010
Kyriacos Souroullas <kyriacos.souroullas@gmail.com>
2019-12-29 18:23:26 -0800

    Render map

--- main.go
+++ main.go
@@ -2,18 +2,47 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 
 	"github.com/veandco/go-sdl2/sdl"
 )
 
 const (
-	windowWidth  = 800
-	windowHeight = 600
-
 	fps             = 30
 	frameTimeLength = 1000 / fps
+
+	pi    = math.Pi
+	twoPI = 2 * pi
+
+	tileSize   = 64
+	mapNumRows = 13
+	mapNumCols = 20
+
+	minimapScaleFactor = 0.2
+
+	windowWidth  = mapNumCols * tileSize
+	windowHeight = mapNumRows * tileSize
+
+	fov     = 60 * (math.Pi / 180)
+	numRays = windowWidth
 )
+
+var grid = [mapNumRows][mapNumCols]int{
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+}
 
 var (
 	window   *sdl.Window
@@ -21,9 +50,21 @@ var (
 
 	running               = false
 	ticksLastFrame uint32 = 0
-
-	playerX, playerY = 0, 0
 )
+
+// Player - stuff
+type Player struct {
+	x, y          float64
+	width, height float64
+
+	turnDirection int // -1 left, +1 right
+	walkDirection int // -1 left, +1 right
+
+	rotationAngle float64
+
+	walkSpeed float64
+	turnSpeed float64
+}
 
 func run() (err error) {
 	if err = sdl.Init(sdl.INIT_EVERYTHING); err != nil {
@@ -37,7 +78,7 @@ func run() (err error) {
 		sdl.WINDOWPOS_CENTERED,
 		windowWidth,
 		windowHeight,
-		sdl.WINDOW_BORDERLESS)
+		sdl.WINDOW_OPENGL)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error creating SDL window: %s\n", err)
 		return
@@ -65,20 +106,30 @@ func destroy() {
 }
 
 func setup() {
-
+	// initialize the player
+	// player := &Player{
+	// 	x:             windowWidth / 2,
+	// 	y:             windowHeight / 2,
+	// 	width:         5,
+	// 	height:        5,
+	// 	turnDirection: 0,
+	// 	walkDirection: 0,
+	// 	rotationAngle: pi / 2,
+	// 	walkSpeed:     100,
+	// 	turnSpeed:     45 * (pi / 180),
+	// }
 }
 
 func update() {
-	// stop and waste some time until we reach the target frame time length we want
-	// timeout = SDL_GetTicks() + frameTimeLength
-	// !SDL_TICKS_PASSED(SDL.GetTicks(), timeout)
+	/* stop and waste some time until we reach the target frame time length we want
+	 * timeout = SDL_GetTicks() + frameTimeLength
+	 * !SDL_TICKS_PASSED(SDL.GetTicks(), timeout)
+	 */
 	sdl.Delay(frameTimeLength)
 
-	deltaTime := float64(sdl.GetTicks()-ticksLastFrame) / 1000.0
-	ticksLastFrame = sdl.GetTicks()
+	// deltaTime := float64(sdl.GetTicks()-ticksLastFrame) / 1000.0
+	// ticksLastFrame = sdl.GetTicks()
 
-	playerX += int(50.0 * deltaTime)
-	playerY += int(50.0 * deltaTime)
 }
 
 func render() {
@@ -86,13 +137,39 @@ func render() {
 	renderer.Clear() // clear back buffer
 
 	// render all game objects for current frame
-	renderer.SetDrawColor(255, 255, 0, 255)
-	rect := sdl.Rect{X: int32(playerX), Y: int32(playerY), W: 20, H: 20}
-	renderer.FillRect(&rect)
+	renderMap()
+	renderPlayer()
 
 	// swap current buffer with back buffer
 	renderer.Present()
 }
+
+func renderMap() {
+	for i := 0; i < mapNumRows; i++ {
+		for j := 0; j < mapNumCols; j++ {
+			tileX := j * tileSize // column
+			tileY := i * tileSize // row
+
+			var tileColor uint8 = 0
+			if grid[i][j] != 0 {
+				tileColor = 255
+			}
+
+			renderer.SetDrawColor(tileColor, tileColor, tileColor, 255)
+			rect := &sdl.Rect{
+				X: int32(minimapScaleFactor * float64(tileX)),
+				Y: int32(minimapScaleFactor * float64(tileY)),
+				W: int32(math.Floor(minimapScaleFactor * tileSize)),
+				H: int32(math.Floor(minimapScaleFactor * tileSize)),
+			}
+
+			renderer.FillRect(rect)
+		}
+	}
+
+}
+
+func renderPlayer() {}
 
 func processInput() {
 	if event := sdl.PollEvent(); event != nil {
