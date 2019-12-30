1ce752348ebdf5c7bbfafa41059af4f8e18c9c99
Kyriacos Souroullas <kyriacos.souroullas@gmail.com>
2019-12-29 22:28:35 -0800

    Draw and move the player - without collision detection

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ const (
 	mapNumRows = 13
 	mapNumCols = 20
 
-	minimapScaleFactor = 0.2
+	minimapScaleFactor = 1.0
 
 	windowWidth  = mapNumCols * tileSize
 	windowHeight = mapNumRows * tileSize
@@ -50,6 +50,8 @@ var (
 
 	running               = false
 	ticksLastFrame uint32 = 0
+
+	player *Player
 )
 
 // Player - stuff
@@ -64,6 +66,65 @@ type Player struct {
 
 	walkSpeed float64
 	turnSpeed float64
+}
+
+func movePlayer(deltaTime float64) {
+	// Turning: its the turn direction -1/+1/0 multiplied by the rotation speed
+	player.rotationAngle += float64(player.turnDirection) * player.turnSpeed * deltaTime
+
+	// Moving:  direction -1/+1/0 multiplied by how fast (speed)
+	//          the player should move to calculate how much of a jump/step we make
+	moveStep := float64(player.walkDirection) * player.walkSpeed * deltaTime
+
+	newX := player.x + math.Cos(player.rotationAngle)*moveStep
+	newY := player.y + math.Sin(player.rotationAngle)*moveStep
+
+	player.x = newX
+	player.y = newY
+	// perform wall collision check
+}
+
+func renderMap() {
+	for i := 0; i < mapNumRows; i++ {
+		for j := 0; j < mapNumCols; j++ {
+			tileX := j * tileSize // column
+			tileY := i * tileSize // row
+
+			var tileColor uint8 = 0
+			if grid[i][j] != 0 {
+				tileColor = 255
+			}
+
+			renderer.SetDrawColor(tileColor, tileColor, tileColor, 255)
+			rect := &sdl.Rect{
+				X: int32(minimapScaleFactor * float64(tileX)),
+				Y: int32(minimapScaleFactor * float64(tileY)),
+				W: int32(math.Floor(minimapScaleFactor * tileSize)),
+				H: int32(math.Floor(minimapScaleFactor * tileSize)),
+			}
+			renderer.FillRect(rect)
+		}
+	}
+
+}
+
+func renderPlayer() {
+	renderer.SetDrawColor(255, 255, 255, 255)
+	playerRect := &sdl.Rect{
+		X: int32(minimapScaleFactor * player.x),
+		Y: int32(minimapScaleFactor * player.y),
+		W: int32(minimapScaleFactor * player.width),
+		H: int32(minimapScaleFactor * player.height),
+	}
+	renderer.FillRect(playerRect)
+
+	// draw line to show direction the player is looking
+	renderer.DrawLine(
+		int32(minimapScaleFactor*player.x),
+		int32(minimapScaleFactor*player.y),
+		int32(minimapScaleFactor*player.x+math.Cos(player.rotationAngle)*40),
+		int32(minimapScaleFactor*player.y+math.Sin(player.rotationAngle)*40),
+	)
 }
 
 func run() (err error) {
@@ -107,17 +168,17 @@ func destroy() {
 
 func setup() {
 	// initialize the player
-	// player := &Player{
-	// 	x:             windowWidth / 2,
-	// 	y:             windowHeight / 2,
-	// 	width:         5,
-	// 	height:        5,
-	// 	turnDirection: 0,
-	// 	walkDirection: 0,
-	// 	rotationAngle: pi / 2,
-	// 	walkSpeed:     100,
-	// 	turnSpeed:     45 * (pi / 180),
-	// }
+	player = &Player{
+		x:             windowWidth / 2,
+		y:             windowHeight / 2,
+		width:         1,
+		height:        1,
+		turnDirection: 0,
+		walkDirection: 0,
+		rotationAngle: pi / 2,
+		walkSpeed:     100,
+		turnSpeed:     70 * (pi / 180),
+	}
 }
 
 func update() {
@@ -127,9 +188,10 @@ func update() {
 	 */
 	sdl.Delay(frameTimeLength)
 
-	// deltaTime := float64(sdl.GetTicks()-ticksLastFrame) / 1000.0
-	// ticksLastFrame = sdl.GetTicks()
+	deltaTime := float64(sdl.GetTicks()-ticksLastFrame) / 1000.0
+	ticksLastFrame = sdl.GetTicks()
 
+	movePlayer(deltaTime)
 }
 
 func render() {
@@ -144,42 +206,35 @@ func render() {
 	renderer.Present()
 }
 
-func renderMap() {
-	for i := 0; i < mapNumRows; i++ {
-		for j := 0; j < mapNumCols; j++ {
-			tileX := j * tileSize // column
-			tileY := i * tileSize // row
-
-			var tileColor uint8 = 0
-			if grid[i][j] != 0 {
-				tileColor = 255
-			}
-
-			renderer.SetDrawColor(tileColor, tileColor, tileColor, 255)
-			rect := &sdl.Rect{
-				X: int32(minimapScaleFactor * float64(tileX)),
-				Y: int32(minimapScaleFactor * float64(tileY)),
-				W: int32(math.Floor(minimapScaleFactor * tileSize)),
-				H: int32(math.Floor(minimapScaleFactor * tileSize)),
-			}
-
-			renderer.FillRect(rect)
-		}
-	}
-
-}
-
-func renderPlayer() {}
-
 func processInput() {
 	if event := sdl.PollEvent(); event != nil {
-		switch k := event.(type) {
+		switch t := event.(type) {
 		case *sdl.QuitEvent: // sdl.QUIT
 			// println("Quit")
 			running = false
 		case *sdl.KeyboardEvent:
-			if k.Keysym.Sym == sdl.K_ESCAPE {
-				running = false
+			key := t.Keysym.Sym
+			if t.Type == sdl.KEYDOWN {
+				switch key {
+				case sdl.K_ESCAPE:
+					running = false
+				case sdl.K_UP:
+					player.walkDirection = 1
+				case sdl.K_DOWN:
+					player.walkDirection = -1
+				case sdl.K_RIGHT:
+					player.turnDirection = 1
+				case sdl.K_LEFT:
+					player.turnDirection = -1
+				}
+			}
+			if t.Type == sdl.KEYUP {
+				switch key {
+				case sdl.K_UP, sdl.K_DOWN:
+					player.walkDirection = 0
+				case sdl.K_RIGHT, sdl.K_LEFT:
+					player.turnDirection = 0
+				}
 			}
 		}
 	}
